1a3a0db7fbdf4a785582897ca2a69eb566ddb3c2
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-02-11 16:03:46 +0530

    final

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const route = require('./routes/route')
+const mongoose = require('mongoose')
+var cors = require('cors')
+const multer= require("multer");
+const { AppConfig } = require('aws-sdk');
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use( multer().any())
+
+mongoose.set('strictQuery', false)
+mongoose.connect("mongodb+srv://group12Database:group12Database@bookman.wjkwpbq.mongodb.net/group12Database", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDB is connected"))
+    .catch(err => console.log(err))
+
+app.use("/", route)
+
+app.listen(3000, function () {
+    console.log("Express app running on port 3000")
+})
